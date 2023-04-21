d021235938cc6c95ea1d41633cc8ae94eafc0a4b
dzemiachkovskii <fennyboy2012@gmail.com>
2023-04-21 14:39:27 +0300

    removed def main()

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 
-
-def main():
-    if len(sys.argv) < 2:
-        print('Укажите файл с исходными данными!')
-        return
-    print(f'Файл: {sys.argv[1]}')
+if len(sys.argv) < 2:
+    print('Укажите файл с исходными данными!')
+    exit(1)
+print(f'Файл: {sys.argv[1]}')
